c1afc02ed811eac1f545d5aea4da764b7352b96b
Evgeniy Kirov <evg.kirov@gmail.com>
2011-12-24 19:05:01 +0400

    Избавляемся от надписи "alpha".

--- src/defines.h
+++ src/defines.h
@@ -12,7 +12,7 @@
 
 #define APP_TITLE u("Твоёрадио")
 #define APP_NAME "tvoeradio-desktop"
-#define APP_VERSION "2.0-alpha"
+#define APP_VERSION "2.0"
 #define APP_ORG "tvoeradio"
 #define APP_DOMAIN "tvoeradio.org"
 #define APP_URL QUrl("http://tvoeradio.extractor.locum.ru/app/")
